082a0b0efe2ae7567037c8b9575fff406f182d15
zitzmah <haleyzitzmann@gmail.com>
2023-12-16 05:01:32 -0700

    drinks object added to drinks.js and exported to server.js

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 //     DEPENDENCIES
 //******************************* */
 const express = require("express")
+//import drinks
+const drinks = require("./models/drinks.js")
 const app = express()
 
 //******************************* */
@@ -9,6 +11,13 @@ const app = express()
 //******************************* */
 app.get("/", (req, res) => {
     res.send("Welcome to the Gitpub App!")
+})
+
+app.get("/drinks", (req, res) => {
+    // res.send(fruits)
+    // "index.ejs" => "./views/index.ejs"
+    // {fruits} => {fruits:fruits}
+    res.send(drinks)
 })
 
 //******************************* */
